1abc518aa75d25e6419dc4f225fa2d0b30e56525
justinachen <jjc382@cornell.edu>
2018-04-15 11:42:17 -0400

    updated clean_data.py to match jupyter nb

--- Gui/clean_data.py
+++ Gui/clean_data.py
@@ -64,10 +64,9 @@ def create_teacher_info(course_info, out_file_dir):
 	return add
 
 
-
-def create_LP_input(full_course_info, origional_course_input):
+def create_LP_input(full_course_info, original_course_input):
 	"""
-	Creates the pandas dataframe that serces as the main input for the LP
+	Creates the pandas dataframe that serves as the main input for the LP
 	this works off of the dataframe crated by `create_teacher_info`
 
 	Parameters
@@ -75,12 +74,12 @@ def create_LP_input(full_course_info, origional_course_input):
 	full_course_info - The dataframe produced by create_teacher_info
 						(has the multiple instances listed)
 
-	origional_course_input - the origional file the school is to fill out
+	original_course_input - the original file the school is to fill out
 							 i.e., the course_info argument to create_teacher_info
 	"""
 	add = full_course_info # per the way Justina wrote her code
 
-	all_info = pd.concat([origional_course_input, add], axis=0).sort_values(by=['Course Name']).reset_index(drop=True)
+	all_info = pd.concat([original_course_input, add], axis=0).sort_values(by=['Course Name']).reset_index(drop=True)
 
 	# add in double periods 
 	add_double = pd.DataFrame() 
@@ -90,18 +89,87 @@ def create_LP_input(full_course_info, origional_course_input):
 	    c = row['Course Name'] + ' II'
 	    updated_row = row.copy()
 	    updated_row['Course Name'] = c
+	    updated_row['Double Period'] = 0
 	    add_double = add_double.append(updated_row, ignore_index=True)    
-	add_double
 	all_info = pd.concat([all_info, add_double], axis=0).sort_values(by=['Course Name'])
 
-	# convert "yes" to 1
-	all_info['Double Period'] = all_info['Double Period'].map({'Yes': 1})
+	# convert "yes" to 1, keep 0
+	all_info['Double Period'] = all_info['Double Period'].map({'Yes': 1, 0:0})
+	all_info.reset_index(drop=True)
+	all_info.to_csv(file_location + '/LP_Input.csv', index=False)
 
 	return all_info
 
 
+def dept_proximity(file_location, input_file):
+	"""
+	Creates the proximity matrix that indicates if a course belongs to a department. 
+	Columns are departments, rows are all courses. 1 if the course is in the dept, 0
+	otherwise. The second half of double periods are considered 0. 
+
+	Parameters
+	----------
+	file_location - The directory where the input_file is located, and where the output file
+					Proximity.csv with the proximity matrix will be saved. 
+
+	input_file - the LP input file from which the proximity matrix will be constructed. 
+					Needs to be in the file_location directory
+	"""
+					
+    # columns are department, rows are course names
+    info = pd.read_csv(file_location + input_file, delimiter=',')
+
+    depts = info[["Course Name", "MS Category", "HS Category"]]
+    depts = depts.fillna(0)
+
+    # iterate over depts
+    msd = set(depts["MS Category"])
+    msd.remove(0)
+
+    hsd = set(depts["HS Category"])
+    hsd.remove(0)
+
+    cols = msd.union(hsd)
+    single = [subject for subject in cols if '&' not in subject]
+    double = [subject for subject in cols if '&' in subject]
+
+    sim = pd.DataFrame(index=depts['Course Name'], columns=single)
+
+    # 2nd period of double courses
+    second = np.array(info.loc[info['Double Period'] == 0]['Course Name'])
+
+    for d in cols:  
+        dept_courses_MS = depts["Course Name"].loc[depts["MS Category"] == d]
+        dept_courses_HS = depts["Course Name"].loc[depts["HS Category"] == d]
+        dept_courses = dept_courses_MS.append(dept_courses_HS, ignore_index=True)
+        for course in dept_courses:
+
+            # set all 2nd part in double period to 0
+            if course in second:
+                if '&' in d:
+                    d1 = d.split(" ")[0]
+                    d2 = d.split(" ")[2]
+
+                    sim.loc[course, d1]=0
+                    sim.loc[course, d2]=0
+
+                else:
+                    sim.loc[course, d] = 0
+
+            elif d in single: 
+                sim.loc[course, d]=1
+
+            else: #d in double
+                # get two depts
+                d1 = d.split(" ")[0]
+                d2 = d.split(" ")[2]
+
+                sim.loc[course, d1]=1
+                sim.loc[course, d2]=1
 
 
+    sim = sim.fillna(0)            
+    sim.to_csv(file_location + '/Proximity.csv')
 
 
 
